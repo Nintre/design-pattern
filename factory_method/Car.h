1f4c310e82ac3e4b24663be440936a26ef385fcc
Nintre <313275847@qq.com>
2021-08-22 15:36:07 +0800

    工厂方法模式

---
+++ factory_method/Car.h
@@ -0,0 +1,13 @@
+#ifndef CAR_H
+#define CAR_H
+#include<iostream>
+using namespace::std;
+class Car
+{
+public:    
+    virtual void Name()
+    {
+
+    }
+};
+#endif
---
+++ factory_method/Car_factory.h
@@ -0,0 +1,15 @@
+#ifndef CAR_FACTORY_H
+#define CAR_FACTORY_H
+#include"Car.h"
+
+class CarFactory
+{
+public:
+	virtual void product_factory()
+	{
+
+	}
+};
+
+#endif // !CAR_FACTORY_H
+
